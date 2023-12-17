e78522d9895ef68d49994f7d44eaf3324e65dc35
Martin Wagenbach <martinwagenbach@outlook.de>
2023-12-17 21:34:37 +0100

    Operatoren

---
+++ src/main/java/de/mimnu/school/operatoren/Aufgabe2.java
@@ -0,0 +1,11 @@
+package de.mimnu.school.operatoren;
+
+public class Aufgabe2 {
+    
+    public static void main(String args[]) {
+		int a = 11, b = 2;
+		
+		//Wie der gezeigte Wert darstellt, berechnet man mit dem Modulo "%" Operator den Rest.
+		System.out.println(a + " % " + b + " = " + (a%b));
+	}
+}
---
+++ src/main/java/de/mimnu/school/operatoren/Aufgabe3.java
@@ -0,0 +1,15 @@
+package de.mimnu.school.operatoren;
+
+public class Aufgabe3 {
+    
+    public static void main(String args[]) {
+		int a = 11, b = 2;
+		
+		/*
+		 * Die sogenannten Verbundoperatoren vereinfachen die Schreibweise.
+		 * Außerdem wird der Wert in der Variable "a" gespeichert.
+		 */
+		System.out.println(a+=b);
+		System.out.println(a-=b);
+	}
+}
